36b7528cc956a05352b43cbd5243b88589dea464
Iris Membrere <irismembrere@gmail.com>
2023-04-19 10:45:43 +0800

    Checking for the correspodning symbol to substitute

--- Assignment 2.2_Decryption.py
+++ Assignment 2.2_Decryption.py
@@ -7,3 +7,23 @@
 # Get an encrypted input from the user
 encrypted_input = input("Input the encrypted text: ").lower()
 decrypted_output = ""
+
+# Check for the corresponding symbol to substitute
+for i in range(len(encrypted_input)):
+# If there is '*', change to 'a'
+    if encrypted_input[i] == '*':
+        decrypted_output += 'a'
+# If there is '&', change to 'e'
+    elif encrypted_input[i] == '&':
+        decrypted_output += 'e'
+# If there is '#', change to 'i'
+    elif encrypted_input[i] == '#':
+        decrypted_output += 'i'
+# If there is '+', change to 'o'
+    elif encrypted_input[i] == '+':
+        decrypted_output += 'o'
+# If there is '!', change to 'u'
+    elif encrypted_input[i] == '!':
+        decrypted_output += 'u'
+    else:
+        decrypted_output += encrypted_input[i]
