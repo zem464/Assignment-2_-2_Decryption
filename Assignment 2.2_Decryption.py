f23ceedb6327129f8968373882dc030f7f032fd2
Iris Membrere <irismembrere@gmail.com>
2023-04-19 10:43:51 +0800

    Getting an encrypted input from the user

---
+++ Assignment 2.2_Decryption.py
@@ -0,0 +1,9 @@
+# Zemerelin Iris M. Membrere
+# BSCPE 1-4
+# Assignment 2
+
+# Problem 2 - Decryption
+
+# Get an encrypted input from the user
+encrypted_input = input("Input the encrypted text: ").lower()
+decrypted_output = ""
